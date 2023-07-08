ca8c81810470d9456c7cd750bbf15277a525bbdd
Dmitry Mozgin <m0391n@gmail.com>
2023-07-08 23:12:09 +0300

    Add glfw3 sample code

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
+#include <glbinding/gl/gl.h>
+#include <glbinding/glbinding.h>
+#include <GLFW/glfw3.h>
 
-int main(int, char**) {
-    std::cout << "Hello, from NoiseSahderSandbox!\n";
+#undef GL_COLOR_BUFFER_BIT
+
+int main(int argc, char *argv[])
+{
+    glfwInit();
+    auto window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glbinding::initialize(glfwGetProcAddress);
+
+    gl::glClearColor(0.5, 0.5, 0.5, 1);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        gl::glClear(gl::GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 }
